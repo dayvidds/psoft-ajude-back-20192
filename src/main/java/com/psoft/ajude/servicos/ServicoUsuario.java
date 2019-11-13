83a460d2a7dde2e5cec713bc9bad68becea7bc00
Pedro Espindula <joaopedro.jpse@gmail.com>
2019-11-13 16:47:38 -0300

    Adiciona Autowired para todos os servicos

--- src/main/java/com/psoft/ajude/servicos/ServicoUsuario.java
+++ src/main/java/com/psoft/ajude/servicos/ServicoUsuario.java
@@ -2,6 +2,7 @@ package com.psoft.ajude.servicos;
 
 import com.psoft.ajude.daos.RespositorioUsuario;
 import com.psoft.ajude.entidades.Usuario;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -9,7 +10,9 @@ import java.util.Optional;
 @Service
 public class ServicoUsuario {
 
+    @Autowired
     private RespositorioUsuario<Usuario, String> usuariosDAO;
+    @Autowired
     private ServicoEmail emailService;
 
     public ServicoUsuario(RespositorioUsuario<Usuario, String> usuariosDAO){
