c581e2e87c3aecc57192baa4ec56e3a349bc5d15
dayvidds <dayvid.silva@ccc.ufcg.edu.br>
2019-11-18 14:35:30 -0300

    Realiza ajustes de anotacoes nos controladores

--- src/main/java/com/psoft/ajude/controladores/ControladorUsuarios.java
+++ src/main/java/com/psoft/ajude/controladores/ControladorUsuarios.java
@@ -7,15 +7,17 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/usuario")
 public class ControladorUsuarios {
 
     @Autowired
     private ServicoUsuario servicoUsuario;
 
-    @PostMapping("/cadastro")
+    @PostMapping("/")
     public ResponseEntity<Usuario> cadastraUsuario(@RequestBody Usuario usuario) {
         return new ResponseEntity<Usuario>(servicoUsuario.cadastraUsuario(usuario), HttpStatus.CREATED);
     }
--- src/main/java/com/psoft/ajude/daos/RespositorioUsuario.java
+++ src/main/java/com/psoft/ajude/daos/RespositorioUsuario.java
@@ -2,10 +2,12 @@ package com.psoft.ajude.daos;
 
 import com.psoft.ajude.entidades.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.io.Serializable;
 import java.util.Optional;
 
+@Repository
 public interface RespositorioUsuario<T, ID extends Serializable> extends JpaRepository<Usuario, String> {
 
 
