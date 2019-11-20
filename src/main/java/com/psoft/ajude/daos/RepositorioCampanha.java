ad81692f7d67643f64d5df3c8e6e5810a2c7dd44
dayvidds <dayvid.silva@ccc.ufcg.edu.br>
2019-11-20 17:12:35 -0300

    Adiciona metodo de pesquisa por descricao no DAO

--- src/main/java/com/psoft/ajude/daos/RepositorioCampanha.java
+++ src/main/java/com/psoft/ajude/daos/RepositorioCampanha.java
@@ -5,7 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.io.Serializable;
+import java.util.List;
 
 @Repository
 public interface RepositorioCampanha<T, ID extends Serializable> extends JpaRepository<Campanha, Integer> {
+    List<Campanha> findByNomeCurtoIgnoreCaseContaining(String parametro);
 }
