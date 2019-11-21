87d52b9ebebac8d34122fec5a2a796a18b95bc64
dayvidds <dayvid.silva@ccc.ufcg.edu.br>
2019-11-21 15:27:56 -0300

    Adiciona método que retorna campanha pela URL

--- src/main/java/com/psoft/ajude/servicos/ServicoCampanha.java
+++ src/main/java/com/psoft/ajude/servicos/ServicoCampanha.java
@@ -6,13 +6,13 @@ import com.psoft.ajude.dtos.DTOCampanha;
 import com.psoft.ajude.dtos.DTOPesquisa;
 import com.psoft.ajude.dtos.DTOUsuario;
 import com.psoft.ajude.entidades.Campanha;
-import com.psoft.ajude.entidades.StatusCampanha;
 import com.psoft.ajude.entidades.Usuario;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.servlet.ServletException;
 import java.util.List;
-import java.util.Set;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -41,5 +41,14 @@ public class ServicoCampanha {
         return campanhaDAO.findByNomeCurtoIgnoreCaseContaining(dtoPesquisa.getParametro()).stream()
                 .filter(campanha -> dtoPesquisa.getStatusProcurados().contains(campanha.getStatusCampanha()))
                 .collect(Collectors.toList());
+    }
+
+    public Campanha retornaCampanha(String urlCampanha) throws ServletException {
+        Optional<Campanha> optionalCampanha = campanhaDAO.findById(urlCampanha);
+
+        if (!optionalCampanha.isPresent()) {
+            throw new ServletException("Campanha não cadastrada");
+        }
+        return optionalCampanha.get();
     }
 }
