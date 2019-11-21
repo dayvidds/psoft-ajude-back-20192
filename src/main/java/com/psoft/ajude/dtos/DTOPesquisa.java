ab6e00a90713bb0adde973d3a9977ebe2873fa00
Pedro Espindula <joaopedro.jpse@gmail.com>
2019-11-21 14:26:41 -0300

    Adiciona filtro de status na pesquisa de campanha

--- src/main/java/com/psoft/ajude/dtos/DTOPesquisa.java
+++ src/main/java/com/psoft/ajude/dtos/DTOPesquisa.java
@@ -1,15 +1,23 @@
 package com.psoft.ajude.dtos;
 
 
+import com.psoft.ajude.entidades.StatusCampanha;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
+
+import java.util.Set;
 
 @AllArgsConstructor
 @NoArgsConstructor
 public class DTOPesquisa {
     private String parametro;
+    private Set<StatusCampanha> statusProcurados;
 
     public String getParametro() {
         return parametro;
+    }
+
+    public Set<StatusCampanha> getStatusProcurados() {
+        return statusProcurados;
     }
 }
--- src/main/java/com/psoft/ajude/servicos/ServicoCampanha.java
+++ src/main/java/com/psoft/ajude/servicos/ServicoCampanha.java
@@ -3,13 +3,17 @@ package com.psoft.ajude.servicos;
 import com.psoft.ajude.daos.RepositorioCampanha;
 import com.psoft.ajude.daos.RepositorioUsuario;
 import com.psoft.ajude.dtos.DTOCampanha;
+import com.psoft.ajude.dtos.DTOPesquisa;
 import com.psoft.ajude.dtos.DTOUsuario;
 import com.psoft.ajude.entidades.Campanha;
+import com.psoft.ajude.entidades.StatusCampanha;
 import com.psoft.ajude.entidades.Usuario;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 public class ServicoCampanha {
@@ -33,7 +37,9 @@ public class ServicoCampanha {
         return new DTOCampanha(campanha, new DTOUsuario(usuario));
     }
 
-    public List<Campanha> pesquisarCampanha(String parametro) {
-        return campanhaDAO.findByNomeCurtoIgnoreCaseContaining(parametro);
+    public List<Campanha> pesquisarCampanha(DTOPesquisa dtoPesquisa) {
+        return campanhaDAO.findByNomeCurtoIgnoreCaseContaining(dtoPesquisa.getParametro()).stream()
+                .filter(campanha -> dtoPesquisa.getStatusProcurados().contains(campanha.getStatusCampanha()))
+                .collect(Collectors.toList());
     }
 }
