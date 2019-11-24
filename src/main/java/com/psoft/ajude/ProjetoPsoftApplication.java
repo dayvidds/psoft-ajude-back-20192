fe49da24ddcc97319fd04ffb5e9bb8d26fdd37fb
dayvidds <dayvid.silva@ccc.ufcg.edu.br>
2019-11-24 14:47:23 -0300

    Adiciona o Swagger

--- src/main/java/com/psoft/ajude/ProjetoPsoftApplication.java
+++ src/main/java/com/psoft/ajude/ProjetoPsoftApplication.java
@@ -10,8 +10,10 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class ProjetoPsoftApplication {
 
     public static void main(String[] args) {
