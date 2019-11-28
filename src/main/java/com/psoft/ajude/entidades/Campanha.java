a743b9d2a196f72a7eb705fc7ce45f9a36acac1a
Pedro Espindula <joaopedro.jpse@gmail.com>
2019-11-27 22:43:49 -0300

    Adiciona import que faltava

--- src/main/java/com/psoft/ajude/entidades/Campanha.java
+++ src/main/java/com/psoft/ajude/entidades/Campanha.java
@@ -5,10 +5,7 @@ import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
+import javax.persistence.*;
 import java.util.*;
 import java.util.stream.Collectors;
 
